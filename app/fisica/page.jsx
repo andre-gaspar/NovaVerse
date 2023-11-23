5e8d232f8a5853a961ea38a9dbdb9fa9a513e745
andre-gaspar <ap.gaspar@campus.fct.unl.pt>
2023-11-23 02:06:31 +0000

    phisics working with a button for movement 5

--- app/fisica/page.jsx
+++ app/fisica/page.jsx
@@ -48,6 +48,7 @@ export default function Page() {
           cursor: 'pointer',
           userSelect: 'none',
           touchAction: 'manipulation',
+          userSelect: 'none',
         }}
         onTouchStart={startCubeJumpTimer}
         onTouchEnd={stopCubeJumpTimer}
